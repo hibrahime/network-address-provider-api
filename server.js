34be7f4f30fc60e067888ec01bb2c566ae1cde2c
halil.eryildiz <halil.eryildiz@inavitas.com>
2021-03-19 01:59:45 +0300

    first

---
+++ server.js
@@ -0,0 +1,58 @@
+const express = require('express');
+const address = require('address');
+
+const app = express();
+const port = 3000;
+
+function returnDefaultText(res) {
+  const infoText = 'use /address?type=all or /address?type=ip or /address?type=ipv6 or /address?type=mac or /address?type=dns for now!';
+  res.send(infoText);
+}
+
+function callBackWithError(res) {
+  return (err, addr) => {
+    if (err) {
+      res.send(err);
+    } else {
+      res.json(addr);
+    }
+  };
+}
+
+app.get('/address', (req, res) => {
+  const { type, parameters } = req.query;
+
+  switch (type) {
+    case 'all':
+      address(parameters, callBackWithError(res));
+      break;
+    case 'ip':
+      res.json(address.ip(parameters));
+      break;
+    case 'ipv6':
+      res.json(address.ipv6());
+      break;
+    case 'mac':
+      address.mac(parameters, callBackWithError(res));
+      break;
+    case 'interface':
+      // res.json(address.interface('IPv4', 'eth1');
+      returnDefaultText(res);
+      break;
+    case 'dns':
+      address.dns(callBackWithError(res));
+      break;
+    default:
+      returnDefaultText(res);
+      break;
+  }
+
+});
+
+app.get('/*', (req, res) => {
+  returnDefaultText(res);
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});

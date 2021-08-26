823c7aea1715f48140c093751bb4102364aecbd2
halil.eryildiz <halil.eryildiz@inavitas.com>
2021-08-26 12:02:36 +0300

    some changes not ready

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const address = require('address');
+const os = require('os');
 
+os.networkInterfaces()
 const app = express();
-const port = 3000;
+const port = 3005;
 
 function returnDefaultText(res) {
   const infoText = 'use /address?type=all or /address?type=ip or /address?type=ipv6 or /address?type=mac or /address?type=dns for now!';
@@ -22,10 +24,31 @@ function callBackWithError(res) {
   };
 }
 
+function prepareResponse({ package, type, parameters }) {
+    console.log('package, type, parameters', package, type, parameters);
+    const packageTypes = {
+        node:{
+
+        },
+        address:{
+            all:(parameters)=> console.log('parameters', parameters),
+            ip:(parameters)=> console.log('parameters', parameters),
+            ipv6:(parameters)=> console.log('parameters', parameters),
+            mac:(parameters)=> console.log('parameters', parameters),
+            interface:(parameters)=> console.log('parameters', parameters),
+            dns:(parameters)=> console.log('parameters', parameters),
+        }
+    }
+    return packageTypes[package][type]
+}
+
+
+
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.get('/address', (req, res) => {
-  const { type, parameters } = req.query;
+  const { package, type, parameters } = req.query;
+  prepareResponse(req.query); 
 
   switch (type) {
     case 'all':
@@ -41,7 +64,6 @@ app.get('/address', (req, res) => {
       address.mac(parameters, callBackWithError(res));
       break;
     case 'interface':
-      // res.json(address.interface('IPv4', 'eth1');
       returnDefaultText(res);
       break;
     case 'dns':
@@ -59,4 +81,5 @@ app.get('/*', (req, res) => {
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
+  console.log(`Example app listening at http://localhost:${port}/api-docs`);
 });
